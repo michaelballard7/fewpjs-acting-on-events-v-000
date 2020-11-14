84ac06b89b1436327dc614954c3f47d1787fcd9c
Michael Ballard <ballard.michael9@gmail.com>
2020-11-14 23:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function moveDodgerRight() {
 
     // set the dogdger movement right , if right <parentElement.clientWidth
 
-    if(right < e.target.parentElement.clientWidth){
+    if(right < 180){
 
         dodger.style.right=`${right + 1}`
     }
