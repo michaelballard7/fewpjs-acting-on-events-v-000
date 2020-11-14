27790d3cc67dcae6e79ed02999abf73d49e0c3a9
Michael Ballard <ballard.michael9@gmail.com>
2020-11-14 23:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ dodger.addEventListener('keydown', function(e){
         // now set the amount for movement
 
         if(dogdger.style.left > 0) {
-            
             dodger.style.left=`${left - 1}`
         }
 
