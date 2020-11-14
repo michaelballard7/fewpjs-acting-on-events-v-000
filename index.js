368dc0c17bf4dab2fe716672ebec09e246e986cd
Michael Ballard <ballard.michael9@gmail.com>
2020-11-14 23:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+const dodger = document.querySelector('#dodger')
+
+
+// add event listner for keydown event
+dodger.addEventListener('keydown', function(e){
+    if(e.key === 'ArrowLeft'){
+        // drop the px
+        let leftNumbers = dodger.style.left.replace("px","")
+
+        // convert string value to number
+        let left = parseInt(leftNumbers, 10)
+
+        // now set the amount for movement
+        dodger.style.left=`${left + 1}`
+    }
+} )
+
+// creeate function for move left
+
+
+// create a function for move right
+
+
+// create conditional to match key type / evebt
+if()
